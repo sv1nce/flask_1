17ad5bdc22e774951f9dff86e694bb3023c5381d
sv1nce <sviatoslav.kushnir.work@gmail.com>
2023-10-13 15:55:53 +0300

    URL fixes

--- app.py
+++ app.py
@@ -9,7 +9,10 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
-    
+
+@app.route('/contactus')
+def contact():
+    return render_template('contact.html')    
 
 if __name__ == '__main__':
     app.run(debug=True)
