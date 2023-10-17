efa1a33270eec9d0e9fc1a54a73ebab73555441d
sv1nce <sviatoslav.kushnir.work@gmail.com>
2023-10-17 15:07:41 +0300

    DB Fixes and Catalog fixes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request,redirect,render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -18,13 +18,31 @@ class Post(db.Model):
 def home():
     return render_template('home.html')
 
+@app.route('/add', methods= ['POST', 'GET'])
+def add():
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['text']
+        
+        post = Post(title = title,text = text)
+        
+        try:
+            db.session.add(post)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return f'While adding a post there was a mistake'
+    else:
+        return render_template('add.html')
+    
+@app.route('/posts')
+def posts():
+    posts = Post.query.all()
+    return render_template('posts.html', posts=posts)
+
 @app.route('/about')
 def about():
     return render_template('about.html')
-
-@app.route('/contactus')
-def contact():
-    return render_template('contact.html')    
-
+   
 if __name__ == '__main__':
     app.run(debug=True)
