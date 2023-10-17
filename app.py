8f592ead95f649181158f6fa4ebf958ae9b9710d
sv1nce <sviatoslav.kushnir.work@gmail.com>
2023-10-17 15:35:22 +0300

    Add changes, so you can add, edit and delete posts

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def add():
         try:
             db.session.add(post)
             db.session.commit()
-            return redirect('/')
+            return redirect('/posts')
         except:
             return f'While adding a post there was a mistake'
     else:
@@ -43,6 +43,37 @@ def posts():
 @app.route('/about')
 def about():
     return render_template('about.html')
-   
+
+@app.route('/post/<int:post_id>')
+def view_post(post_id):
+    post = Post.query.get(post_id)
+    if post :
+        return render_template('post.html', post=post)
+    else:
+        return "Post not found", 404
+
+@app.route('/edit_post/<int:post_id>', methods=['GET', 'POST'])
+def edit_post(post_id):
+    post = Post.query.get(post_id)
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.text = request.form['text']
+        try:
+            db.session.commit()
+            return redirect ('/posts', post_id=post_id)
+        except:
+            return 'An error occurred while editing the post'
+    return render_template('edit_post.html', post=post)
+
+@app.route('/delete_post/<int:post_id>')
+def delete_post(post_id):
+    post = Post.query.get(post_id)
+    try:
+        db.session.delete(post)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return 'An error occurred while deleting the post'
+       
 if __name__ == '__main__':
     app.run(debug=True)
